8d4da4c6f5b142f7fd93e1b83a4de69f5d981b6d
Anurag Deshpande <anuragd60000@gmail.com>
2016-12-19 11:15:18 -0500

    landing page

--- GMailRedesign/controllers/LoginController.js
+++ GMailRedesign/controllers/LoginController.js
@@ -1,43 +1,4 @@
 angular.module('swailMail').controller('LoginController', ['$scope', '$location', 'sharedScope', function ($scope, $location, $sharedScope) {
 
-    $scope.renderSignInButton = function ($sharedScope, $location) {
-        gapi.signin2.render('signInButton',
-            {
-                'scope': 'profile email',
-                'width': 240,
-                'height': 50,
-                'longtitle': true,
-                'theme': 'dark',
-                'onsuccess': function (googleUser) {
-                    var profile = googleUser.getBasicProfile();
-                    $sharedScope.userdata.name = profile.getName();
-                    $sharedScope.userdata.imgUrl = profile.getImageUrl();
-                    $sharedScope.userdata.email = profile.getEmail();
-                    $sharedScope.userdata.id_token = googleUser.getAuthResponse().id_token;
-                    $sharedScope.isLoggedIn = true;
-                    if($sharedScope.isLoggedIn)
-                    {
-                        $location.path('mail');
-                    }
-                    else
-                    {
-                        console.log('Login is false');
-                        console.log($sharedScope);
-                    }
-                },
-                'onfailure': function (error) {
-                    console.log(error);
-                }
-            }
-        );
-    };
 
-    $scope.logout = function () {
-        gapi.auth2.getAuthInstance().signOut().then(function () {
-            console.log('Signed Out');
-        });
-    };
-
-
-    $scope.renderSignInButton($sharedScope, $location);
 }]);
--- GMailRedesign/controllers/MailController.js
+++ GMailRedesign/controllers/MailController.js
@@ -1,21 +1,4 @@
 angular.module('swailMail').controller('MailController', ['$scope','sharedScope','$location', function ($scope, $sharedScope, $location) {
 
-    if(!$sharedScope.isLoggedIn)
-    {
-        $location.path('login');
-    }
-
-    console.log($sharedScope);
-
-
-
-
-    $scope.logout = function () {
-        gapi.auth2.getAuthInstance().signOut().then(function () {
-            console.log('Signed Out');
-        });
-    };
-
-
 
 }] );
